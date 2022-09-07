4f11e30e75e0b7f011a1df5d61c97ea3323edcfa
Antoine Coulon <antoine@coulon.dev>
2022-09-07 10:21:08 +0200

    added a basic interactive user creation form to the codexctl cli

--- cmd/codexctl/codexctl.go
+++ cmd/codexctl/codexctl.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"coulon.dev/codex/internal/configs"
@@ -22,6 +24,10 @@ func init() {
 func main() {
 	flag.Parse()
 
+	if len(flag.Args()) > 1 {
+		log.Fatalln("Too many arguments")
+	}
+
 	cfg, err := configs.LoadServerSettings(configPathFlag)
 	if err != nil {
 		log.Fatalf("could not parse the config file (%s)", err)
@@ -29,19 +35,45 @@ func main() {
 
 	store.InitDB(cfg.DbPath)
 
-	key, err := os.ReadFile("./user.pub")
+	switch flag.Arg(0) {
+	case "new":
+		pubkey := prompt("SSH public key:")
+		u, err := store.NewUser([]byte(pubkey), time.Now())
+		if err != nil {
+			log.Fatalf("could not parse the SSH public key (%s)", err)
+		}
+		name := prompt(fmt.Sprintf("Username (%s):", u.Name))
+		if name != "" {
+			u.Name = name
+		}
+		expInput := strings.TrimSpace(prompt("User expiration day (format: YYYY/MM/DD):"))
+		exp, err := time.Parse("2006/01/02", expInput)
+		if err != nil {
+			log.Fatalf("Invalid expiration date (%s)", err)
+		}
+		u.Expiration = exp
+		principals := prompt("Valid principals (space-separated):")
+		u.Principals = strings.Split(principals, " ")
+		err = u.Save()
+		if err != nil {
+			log.Fatalf("could not save the new user (%s)", err)
+		}
+		fmt.Printf("User %s created successfully\n", u.Name)
+	case "update":
+		log.Fatalln("Not implemented")
+	case "":
+		log.Fatalln("No arguments given")
+	default:
+		log.Fatalln("Unknown subcommand")
+	}
+}
+
+func prompt(label string) (out string) {
+	r := bufio.NewReader(os.Stdin)
+	fmt.Printf("%s ", label)
+	out, err := r.ReadString('\n')
 	if err != nil {
-		log.Fatalf("could not open the public key (%s)", err)
+		log.Fatalf("could not read user input")
 	}
-
-	u, err := store.NewUser(key, time.Now().Add(time.Hour*24*7))
-	if err != nil {
-		log.Fatalf("could not create the user (%s)", err)
-	}
-
-	if err = u.Save(); err != nil {
-		log.Fatalf("could not save the user in the database (%s)", err)
-	}
-
-	fmt.Println("user created successfully")
+	return strings.TrimRight(out, "\n")
 }
--- go.mod
+++ go.mod
@@ -7,4 +7,7 @@ require (
 	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90
 )
 
-require golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+require (
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
+)
